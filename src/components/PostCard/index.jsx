80940bf35f3e5d77135c1ba853cf4c1c87690a82
Gabriel <gabriel.soares14563@gmail.com>
2022-09-08 11:31:11 -0300

    Refatoração separando componentes e paginação

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,9 @@
+export const PostCard = ({title, cover, id, body}) => (
+    <div className='post'>
+        <img src={cover} alt={title}/>
+        <div className="post-content">
+            <h2>{title}</h2>
+            <p>{body}</p>
+        </div>
+    </div>
+);
